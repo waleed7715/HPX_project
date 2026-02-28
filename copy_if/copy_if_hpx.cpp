97325d97acbf09475e0385d4cbad37b2a8208ca8
Waleed Hussain <waleedhussain.7715@gmail.com>
2026-02-28 16:49:23 -0600

    Preparing for benchmark

--- copy_if/copy_if_hpx.cpp
+++ copy_if/copy_if_hpx.cpp
@@ -14,6 +14,7 @@ int hpx_main(hpx::program_options::variables_map& vm)
 {
     int hpx_threads = vm["hpx_threads"].as<int>();
     int input_size = vm["input_size"].as<int>();
+    int num_chunks = vm["num_chunks"].as<int>();
 
     if (hpx_threads <= 0) {
         hpx_threads = hpx::get_num_worker_threads();
@@ -35,32 +36,45 @@ int hpx_main(hpx::program_options::variables_map& vm)
         {
             hpx::execution::experimental::num_cores n_cores(threads);
 
-            // Use the following to define number of chunks
-            // std::size_t chunk_size = size / (threads);
-            // hpx::execution::static_chunk_size(chunk_size) -> passed to the execution policy
+            // Define number of chunks
+            std::size_t chunk_size = 0;
+            if (num_chunks > 0) {
+                std::size_t total_chunks = threads * num_chunks;
+                chunk_size = (size + total_chunks - 1) / total_chunks;
+            }
 
-            std::vector<int> destination(size);
-            
-            auto start = std::chrono::high_resolution_clock::now();
-            
-            auto end_it = hpx::copy_if(hpx::execution::par.with(n_cores), 
-                source.begin(), 
-                source.end(), 
-                destination.begin(), 
-                Pred<int>
-            );
+            auto run = [&](auto exec_policy) {
+                std::vector<int> destination(size);
+                
+                auto start = std::chrono::high_resolution_clock::now();
+                
+                auto end_it = hpx::copy_if(exec_policy,
+                    source.begin(), 
+                    source.end(), 
+                    destination.begin(), 
+                    Pred<int>
+                );
 
-            auto end = std::chrono::high_resolution_clock::now();
+                auto end = std::chrono::high_resolution_clock::now();
 
-            destination.resize(std::distance(destination.begin(), end_it));
+                destination.resize(std::distance(destination.begin(), end_it));
 
-            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
-                end - start).count();
-            
-            std::cout << "Size: " << size
-                << ",  Threads: " << threads
-                << ", Copied elements: " << destination.size() 
-                << ", Duration: " << duration << " us\n";
+                auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+                    end - start).count();
+                
+                std::cout << "Size: " << size
+                    << ",  Threads: " << threads
+                    << ", chunk_size: " << chunk_size
+                    << ", Copied elements: " << destination.size() 
+                    << ", Duration: " << duration << " us\n";
+            };
+
+            if (chunk_size > 0) {
+                run(hpx::execution::par.with(n_cores, hpx::execution::static_chunk_size(chunk_size)));
+            }
+            else {
+                run(hpx::execution::par.with(n_cores));
+            }
         }
     }
 
@@ -75,10 +89,13 @@ int main(int argc, char* argv[])
     desc_commandline.add_options()
         ("hpx_threads",
             hpx::program_options::value<int>()->default_value(0),
-            "Number of cores for num_cores (0 = use all)")
+            "Number of HPX threads (0 = default)")
         ("input_size",
             hpx::program_options::value<int>()->default_value(100'000),
             "Sizes of input vector (default: 100000)")
+        ("num_chunks",
+            hpx::program_options::value<int>()->default_value(0),
+            "Number of chunks (0 = default)")
         ;
     
     hpx::init_params init_args;
